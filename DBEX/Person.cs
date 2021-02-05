79c82fdf236d8dab00aa78bcce4430732e26c7a0
Michiel Wouters <michiel_woute64@hotmail.com>
2021-02-05 14:28:01 +0100

    Intro to DataBinding

---
+++ DBEX/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBEX
+{
+    public class Person
+    {
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public string Hobbies { get; set; }
+
+        public string Summary { get; set; }
+
+        public Person()
+        {
+            Age = 33;
+            Name = "Michiel";
+            Hobbies = "Teaching";
+            Summary = GetSummary();
+        }
+
+        
+
+        private string GetSummary()
+        {
+            return $"{Name} is {Age} years old and likes {Hobbies}";
+        }
+    }
+}
---
+++ DataBindingExample/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace DataBindingExample
+{
+    public class Person
+    {
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public string Hobbies { get; set; }
+
+        public int SelectedValue { get; set; }
+
+        public Person()
+        {
+            Age = 24;
+            Name = "Jack";
+            Hobbies = "Hiking";
+            SelectedValue = 5;
+        }
+    }
+}

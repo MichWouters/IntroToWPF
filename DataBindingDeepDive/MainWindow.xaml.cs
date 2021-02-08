73483648c901aba02cd3ec00a34f56c6c325bb74
Michiel Wouters <michiel_woute64@hotmail.com>
2021-02-08 11:01:59 +0100

    Added simple Databinding example

---
+++ DataBindingDeepDive/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBindingDeepDive
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person myFriendInCodeBehind;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            myFriendInCodeBehind = new Person
+            {
+                Age = 24,
+                Name = "Dave",
+                Residence = "Jakkamakka"
+            };
+
+            // Set binding in code-behind
+            myStackPanel.DataContext = myFriendInCodeBehind;
+        }
+    }
+}
---
+++ DataBindingDeepDive/Person.cs
@@ -0,0 +1,29 @@
+﻿namespace DataBindingDeepDive
+{
+    public class Person
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        private string _residence;
+
+        public string Residence
+        {
+            get { return _residence; }
+            set { _residence = value; }
+        }
+    }
+}
